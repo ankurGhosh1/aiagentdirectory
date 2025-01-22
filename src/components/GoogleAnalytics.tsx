57d5b5b0fe8ebeeecba92a3ae92ba9798524c5f7
ankur-astra <ankur.ghosh@getastra.com>
2025-01-22 23:37:35 +0530

    fixed GA4

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,70 +1,31 @@
-import React, { useState, useEffect } from "react";
-import Script from "next/script";
-
-declare global {
-  interface Navigator {
-    connection?: {
-      saveData?: boolean;
-    };
-  }
-}
-
-const isSpeedTestOrBot = () => {
-  if (typeof window === "undefined") return false; // Return false for SSR
-
-  // Detect connection saver flag (common for PageSpeed tests)
-  const saveData = navigator.connection && navigator.connection.saveData;
-
-  // Detect bot or Google PageSpeed Insights via user-agent
-  const botPatterns = [
-    /bot/i,
-    /crawl/i,
-    /spider/i,
-    /slurp/i,
-    /mediapartners/i,
-    /Lighthouse/i, // Google Lighthouse
-    /Chrome-Lighthouse/i, // Google Chrome Lighthouse
-  ];
-
-  const isBot = botPatterns.some((pattern) =>
-    pattern.test(navigator.userAgent)
-  );
-
-  return saveData || isBot; // Return true if saveData is enabled or a bot is detected
-};
+// components/GoogleAnalytics.tsx
+import { useEffect } from "react";
 
 const GoogleAnalytics = () => {
-  const [shouldLoadGA, setShouldLoadGA] = useState(true);
-
   useEffect(() => {
-    if (isSpeedTestOrBot()) {
-      setShouldLoadGA(false); // Do not load Google Analytics if it's a bot or speed test
-    }
+    if (window.gtag) return;
+
+    // Inject GA4 script into the head tag
+    const script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function () {
+        window.dataLayer && window.dataLayer.push(arguments);
+      };
+      window.gtag("js", new Date());
+
+      // Initialize GA4 with your tracking ID
+      window.gtag("config", process.env.NEXT_PUBLIC_MEASUREMENT_ID, {
+        page_path: window.location.pathname,
+      });
+    };
   }, []);
 
-  if (!shouldLoadGA) {
-    return null; // Skip loading the GA script
-  }
-
-  return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-      />
-
-      <Script id="" strategy="lazyOnload">
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
-      </Script>
-    </>
-  );
+  return null;
 };
 
 export default GoogleAnalytics;
