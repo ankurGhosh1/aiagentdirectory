54ec1e88dca80728e8f3b70339a7188086dfdc3e
ankur-astra <ankur.ghosh@getastra.com>
2025-01-23 20:15:41 +0530

    adds GSC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import GoogleAnalytics from "../components/GoogleAnalytics";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics />
+      <Head>
+        <GoogleAnalytics />
+        <meta
+          name="google-site-verification"
+          content="Iy1NBS2IkZ0mW34o2sSSiyesKPK8cfOXtkzhkAQQeq8"
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
